cf5382b002a49de3d689fbcc90d3dfeaf63d4382
Abbie Weisenbloom <abbiew@froggie.com>
2018-10-22 18:52:54 -0700

    Created React App. Started adding Google Maps image via Utils

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { load_google_maps } from './utils/utils.js';
 
 class App extends Component {
+  state = {}
+
+    componentDidMount() {
+      let googleMapsPromise = load_google_maps();
+      //let placesPromise = load_places();
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="map">
+        </div>
+        <div id="sidebar">
+        </div>
       </div>
     );
   }
