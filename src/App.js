5b64c4ed6170d6a85bc77e1b624863543a6b6948
Abbie Weisenbloom <abbiew@froggie.com>
2018-10-23 21:30:42 -0700

    Added Map object & API object.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
-import { load_google_maps } from './utils/utils.js';
+import { load_google_maps, load_places } from './utils/utils.js';
 
 class App extends Component {
   state = {}
 
     componentDidMount() {
+      //load google maps API asynchronously
       let googleMapsPromise = load_google_maps();
-      //let placesPromise = load_places();
+      let placesPromise = load_places();
+      Promise.all([ //takes in an array of promises
+        googleMapsPromise,
+        placesPromise
+      ])
+     .then(values => {  //an array of values
+        console.log("hi there");
+        console.log(values);  //see the google maps object
+        let google = values[0];  //google 1st,
+        this.venues = values[1].response.venues;
 
+        //const location = {lat: 45.516136, lng: -73.656830};
+        this.google = google;
+        this.markers = [];  //anything google maps easier to deal w/ not in state.
+        this.map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 9,
+            scrollwheel: true,
+            center: { lat: this.venues[0].location.lat, lng: this.venues[0].location.lng }
+        });
+        this.infowindow = new google.maps.InfoWindow();
+     });
   }
+
 
   render() {
     return (
-      <div className="App">
+      <div>
         <div id="map">
         </div>
         <div id="sidebar">
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,33 +1,35 @@
 export function load_google_maps() {
-
   return new Promise(function(resolve, reject) {
-
     // define the global callback that will run when google maps is loaded
-
-    window.resolveGoogleMapsPromise = function() {
-
+      window.resolveGoogleMapsPromise = function() {
       // resolve the google object
-
-      resolve(window.google);
-
+         resolve(window.google);
       // delete the global callback to tidy up since it is no longer needed
-
-      delete window.resolveGoogleMapsPromise;
-
-    }
-
+         delete window.resolveGoogleMapsPromise;
+       }
     // Now, Load the Google Maps API
-
-    const script = document.createElement("script");
-
-    const API_KEY = 'AIzaSyBodL8HXeRSDLsE6-rNdkERs3IE0ORP16E';
-
-    script.src = `https://maps.googleapis.com/maps/api/js?libraries=places&key=${API_KEY}&callback=resolveGoogleMapsPromise`;
-
-    script.async = true;
-
-    document.body.appendChild(script);
-
+       const script = document.createElement("script");
+       const API_KEY = 'AIzaSyBodL8HXeRSDLsE6-rNdkERs3IE0ORP16E';
+       script.src = `https://maps.googleapis.com/maps/api/js?libraries=places&key=${API_KEY}&callback=resolveGoogleMapsPromise`;
+       script.async = true;
+       console.log("I'm in the load_google_maps function");
+       console.log(script.src);
+       document.body.appendChild(script);
   });
 
 }
+
+export function load_places() {
+  let city = 'Silver Spring, MD';
+  let query = 'Music';
+  var apiURL = "https://api.foursquare.com/v2/venues/search?client_id=03WPSRTBRU4O5V1ADWJ133RHXCPZ0ASW5LONKNGFIKCV1U2X&client_secret=CWJOYO2W2RJV0W5G2HIUYGLYUEGQJHDIXWBTDDTPF1HR0DYH&v=20180901&limit=50&near=montreal&query=music"
+  return fetch(apiURL).then(resp => resp.json())
+}
+
+/*export function load_places() {
+  let city = 'Silver Spring, MD';
+  let query = 'Music';
+  var apiURL = "https://api.eventful.com/json/events/search?...&location=San+Diego&app_key=k66RNn8N5hHT9mB7"
+  return fetch(apiURL, {mode:"no-cors"}).then(resp => resp.json())
+}*/
+/*API auth format ---> https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=YYYYMMDD */
