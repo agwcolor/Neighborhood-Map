d3195330237169bf5c7cb816a212e06b8077699b
Abbie Weisenbloom <abbiew@froggie.com>
2018-10-25 17:04:56 -0700

    Added marker window pop up

--- src/App.js
+++ src/App.js
@@ -17,31 +17,56 @@ class App extends Component {
         console.log(values);  //see the google maps object
         let google = values[0];  //google 1st,
         this.venues = values[1].response.venues;
+        console.log(this.venues[0].location.lat + "lat" + this.venues[0].location.lng  + "long" + "is a the this.venues variable");
+
 
         //const location = {lat: 45.516136, lng: -73.656830};
         this.google = google;
         this.markers = [];  //anything google maps easier to deal w/ not in state.
-        this.map = new google.maps.Map(document.getElementById('map'), {
+        let map = new google.maps.Map(document.getElementById('map'), {
             zoom: 9,
             scrollwheel: true,
             center: { lat: this.venues[0].location.lat, lng: this.venues[0].location.lng }});
-        this.infowindow = new google.maps.InfoWindow();
+        let infowindow = new google.maps.InfoWindow();
+        this.map = map;
 
-this.venues.forEach(venue => {   //create marker
-    let marker = new google.maps.Marker({
-      position: { lat: venue.location.lat, lng: venue.location.lng},
-      map: this.map,
-      venue: venue,
-      id: venue.id,
-        name: venue.name,
-      animation: google.maps.Animation.DROP
+        this.venues.forEach(venue => {   //create marker
+         let marker = new google.maps.Marker({
+         position: { lat: venue.location.lat, lng: venue.location.lng},
+         map: this.map,
+         venue: venue,
+         id: venue.id,
+         name: venue.name,
+         animation: google.maps.Animation.DROP
+         });
+         // Create an onclick event to open an infowindow at each marker.
+         marker.addListener('click', function() {
+           populateInfoWindow(this, infowindow);
+         });
+        this.markers.push(marker);
+        //console.log(this.markers)
        });
-    this.markers.push(marker)
 
-  });
 
-     });
-  }
+      });
+      // This function populates the infowindow when the marker is clicked. We'll only allow
+      // one infowindow which will open at the marker that is clicked, and populate based
+      // on that markers position.
+        function populateInfoWindow(marker, infowindow) {
+
+        console.log("this is the value of marker" + marker.id);
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker !== marker) {
+          infowindow.marker = marker;
+          infowindow.setContent('<div>' + marker.name + '</div>');
+          infowindow.open(marker.map, marker);
+          // Make sure the marker property is cleared if the infowindow is closed.
+          infowindow.addListener('closeclick',function(){
+          infowindow.setMarker = null;
+          });
+        }};
+
+}
 
   render() {
     return (
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -10,7 +10,7 @@ export function load_google_maps() {
     // Now, Load the Google Maps API
        const script = document.createElement("script");
        const API_KEY = 'AIzaSyBodL8HXeRSDLsE6-rNdkERs3IE0ORP16E';
-       script.src = `https://maps.googleapis.com/maps/api/js?libraries=places&key=${API_KEY}&callback=resolveGoogleMapsPromise`;
+       script.src = `https://maps.googleapis.com/maps/api/js?libraries=places&key=${API_KEY}&v=3&callback=resolveGoogleMapsPromise`;
        script.async = true;
        console.log("I'm in the load_google_maps function");
        console.log(script.src);
@@ -23,7 +23,9 @@ export function load_places() {
   let city = 'Silver Spring, MD';
   let query = 'Music';
   var apiURL = "https://api.foursquare.com/v2/venues/search?client_id=03WPSRTBRU4O5V1ADWJ133RHXCPZ0ASW5LONKNGFIKCV1U2X&client_secret=CWJOYO2W2RJV0W5G2HIUYGLYUEGQJHDIXWBTDDTPF1HR0DYH&v=20180901&limit=50&near=montreal&query=music"
-  return fetch(apiURL).then(resp => resp.json())
+  return fetch(apiURL, /*{mode: 'no-cors'}*/)
+      .then(resp => resp.json())
+      .catch(alert('no cors!'))
 }
 
 /*export function load_places() {
